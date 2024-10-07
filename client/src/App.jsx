3f1fddcf53d18d2f0073966dbf7fdfb0b4d2e6e5
RobertUlloa <roanuc8@gmail.com>
2024-10-07 16:13:52 -0500

    running and finding bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,21 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
-// Uncomment import statement below after building queries and mutations
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+// Create Apollo Client instance
+const client = new ApolloClient({
+  uri: '/graphql', // This should point to the correct GraphQL endpoint (make sure the proxy is set up correctly in vite.config.js)
+  cache: new InMemoryCache(), // This handles the in-memory cache for performance
+});
 
 function App() {
   return (
-    return (
-      <ApolloProvider client={client}>
-        <div className="flex-column justify-center align-center min-100-vh bg-primary">
-          <Outlet />
-        </div>
-      </ApolloProvider>
-    );
+    // Wrap your application in ApolloProvider to ensure the client is accessible throughout the app
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        <Outlet />
+      </div>
+    </ApolloProvider>
   );
 }
 
